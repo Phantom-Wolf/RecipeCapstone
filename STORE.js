e96d10e139a8d00b78573639344cc5df5e4840ef
Mitchell Schmidt <MitchellT.S@outlook.com>
2019-11-15 17:56:04 -0600

    changed all files, add STORE.js

---
+++ STORE.js
@@ -0,0 +1,37 @@
+const STORE = {
+    exclusions: [
+        "Beef",
+        "Pork",
+        "Chicken",
+        "Meat",
+        "Tofu",
+        "Cheese",
+        "Beans",
+        "Brussels Sprouts",
+        "Carrots",
+        "Cauliflower",
+        "Cilantro",
+        "Mushrooms",
+        "Olives",
+        "Onions",
+        "Tomatoes",
+        "Other"
+    ],
+
+    allergies: [
+        "Dairy",
+        "Egg",
+        "Gluten",
+        "Grain",
+        "Peanut",
+        "Seafood",
+        "Sesame",
+        "Shellfish",
+        "Soy",
+        "Sulfite",
+        "Tree Nut",
+        "Wheat"
+    ]
+
+
+}
--- index.js
+++ index.js
@@ -1,16 +1,61 @@
 
 const apiKey = "21b612134db24ae285c7a2db190c41fc";
-let searchTermString = "italian";
+let searchURL = "https://api.spoonacular.com/recipes/complexSearch";
 
-function newDataAPI() {
+function displayResults(responseJson){
+console.log(responseJson);
+  $('.list').append(`
+  
+  <li>
+    <h2>${responseJson.title}</h2>
+    <div class="short-recipe-container">
+    <img src="${responseJson.image}" alt="${responseJson.title}" class="recipe-image-short">
+    <p>Serving Size: ${responseJson.servings}</p>
+    </div>
+  </li>
+  `);
 
-    let searchTerm = $('#recipe').val();
-    let maxResults = $('#max-results').val();
-    console.log(`search term: ${searchTerm}`)
-    console.log(`max results: ${maxResults}`)
-  fetch(
-    `https://api.spoonacular.com/recipes/complexSearch?query=${searchTerm}&number=${maxResults}&apiKey=21b612134db24ae285c7a2db190c41fc`
-  )
+  $('#results').show();
+
+}
+
+function formatQueryParams(params) {
+  const queryItems = Object.keys(params).map(
+    key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`
+  );
+  return queryItems.join("&");
+}
+
+function getRecipes() {
+
+  let searchTerm = $('#recipe').val();
+
+  let exclusions = [];
+  $('.dropdown-contentA input[type="checkbox"]:checked').each(function(){
+    exclusions.push($(this).val());
+  })
+  let exclude = exclusions.join(',');
+
+  let allergy = [];
+  $('.dropdown-contentB input[type="checkbox"]:checked').each(function(){
+    allergy.push($(this).val());
+  })
+  let allergies = allergy.join(',');
+
+  const params = {
+    apiKey: apiKey,
+    query: searchTerm,
+    number: 2,
+    intolerances: allergies,
+    excludeIngredients: exclude
+    
+  };
+    
+  const queryString = formatQueryParams(params);
+  const url = searchURL + "?" + queryString;
+  console.log(url)
+   
+  fetch(url)
     .then(response => response.json())
     .then(responseJson => getDetails(responseJson))
     .catch(error => console.log(error));
@@ -25,9 +70,28 @@ function getDetails(responseJson) {
       `https://api.spoonacular.com/recipes/${responseJson.results[i].id}/information?includeNutrition=false&apiKey=21b612134db24ae285c7a2db190c41fc`
     )
       .then(response => response.json())
-      .then(responseJson => console.log(responseJson))
+      .then(responseJson => displayResults(responseJson))
       .catch(error => console.log(error));
   }
+}
+
+function populateDropdowns(){
+
+  let exclude = STORE.exclusions;
+  for (let i = 0; i < exclude.length; i++ ){
+    $('#myExclusion').append(`
+    <label class="drop-items ${exclude[i]}"><input type="checkbox" name="exclusion" value="${exclude[i]}">${exclude[i]}</label>
+    `)
+  }
+
+  let allergy = STORE.allergies;
+  for (let i = 0; i < allergy.length; i++ ){
+    $('#myIntolerance').append(`
+    <label class="drop-items ${allergy[i]}"><input type="checkbox" name="exclusion" value="${allergy[i]}">${allergy[i]}</label>
+    `)
+  }
+
+  $('.Other').html
 }
 
 function watchForm() {
@@ -37,13 +101,42 @@ function watchForm() {
     $(".err-js").empty();
     $("#results").hide();
     $(".sorry").remove();
-    newDataAPI();
+    getRecipes();
   });
+
 }
 
 $(function() {
   console.log("App loaded! Waiting for submit!");
   $("#results").hide();
+  populateDropdowns();
   watchForm();
+  
+  console.log(STORE.exclusions)
 });
-// $(newDataAPI(searchTermString));
+
+// creates the toggle effect for dropdown menus
+
+$(function toggleDropdown() {
+
+  $("#exclusion-button").click(function() {
+    $("#myExclusion").toggle();
+  });
+
+  $("#intolerance-button").click(function() {
+    $("#myIntolerance").toggle();
+  });
+
+  $(document).on("click", function(event) {
+    let trigger = $("#exclusion-button")[0];
+    let dropdown = $("#myExclusion");
+    if (dropdown !== event.target && !dropdown.has(event.target).length && trigger !== event.target) {
+      $("#myExclusion").hide();
+    }
+    let trigger2 = $("#intolerance-button")[0];
+    let dropdown2 = $("#myIntolerance");
+    if (dropdown2 !== event.target && !dropdown2.has(event.target).length && trigger2 !== event.target) {
+      $("#myIntolerance").hide();
+    }
+  });
+});
